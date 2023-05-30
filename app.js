c51af36ea1529bc55eac8f0a053ed45de91658d3
jeetuisbest <jeetuchaudhari786@gmail.com>
2023-05-30 18:51:52 +0530

    check

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.post("/webhook", (req, res) => {
     // Check the Incoming webhook message
     console.log(JSON.stringify(req.body, null, 2));
 
+    res.json({
+        "deployed": true
+    })
+
     // info on WhatsApp text message payload: https://developers.facebook.com/docs/whatsapp/cloud-api/webhooks/payload-examples#text-messages
     if (req.body.object) {
         if (
